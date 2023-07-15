b7ec73f5a1c66c147cddbd25f896ac348ed66235
TanuAggarwal <tanuaggarwal0123@gmail.com>
2023-07-16 00:17:08 +0530

    update package.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const env = require("dotenv").config();
 const port = process.env.PORT || 3333;
 
 // Middleware
