53394f3596c0bfb86742f145dbea2d7477f6d223
TanuAggarwal <tanuaggarwal0123@gmail.com>
2023-07-15 23:48:31 +0530

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const port = process.env.PORT || 3333;
 
 // Middleware
 app.use(bodyParser.json({ limit: "10mb" }));
